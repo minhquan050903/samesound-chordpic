47fef904b512d17247e5da6f0db4e6fc0be4ea2e
EgorRevenga <127834856+EgorRevenga@users.noreply.github.com>
2023-03-14 16:22:46 +0300

    Update supabase-admin.ts

--- utils/supabase-admin.ts
+++ utils/supabase-admin.ts
@@ -7,8 +7,8 @@ import { Product, Price, Customer, UserDetails } from "../types";
 // Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
 // as it has admin priviliges and overwrites RLS policies!
 const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string
 );
 
 const upsertProductRecord = async (product: Stripe.Product) => {
