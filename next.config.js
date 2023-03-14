62ff36dfad024a9b0ef63c4d1fb8275eb46e8883
EgorRevenga <127834856+EgorRevenga@users.noreply.github.com>
2023-03-14 13:26:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const { withSentryConfig } = require("@sentry/nextjs");
-
 const moduleExports = {
   reactStrictMode: true,
   swcMinify: true,
-  sentry: {
-    hideSourceMaps: false,
-  },
 };
-
-const sentryWebpackPluginOptions = {
-  silent: true,
-};
-
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
