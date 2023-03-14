38cc053a3bbe96d165b5a685fc7ff7f156b8aee6
EgorRevenga <127834856+EgorRevenga@users.noreply.github.com>
2023-03-14 16:29:56 +0300

    Update pricing.tsx

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -13,16 +13,16 @@ export default function PricingPage({ products }: Props) {
   return <Pricing products={products} />;
 }
 
-export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
+//export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
   //const products = await getActiveProductsWithPrices();
 
-  return {
-    props: {
-      products,
-      title: "Pricing Plans",
-      description:
-        "ChordPic is a free guitar chord diagram creator. You can create beautiful chord diagrams for free. If you want to use ChordPic for commercial purposes, you can upgrade to a paid plan.",
+  //return {
+   // props: {
+   //   products,
+   //   title: "Pricing Plans",
+   //   description:
+       // "ChordPic is a free guitar chord diagram creator. You can create beautiful chord diagrams for free. If you want to use ChordPic for commercial purposes, you can upgrade to a paid plan.",
     },
-    revalidate: 60,
-  };
-}
+  //  revalidate: 60,
+  //};
+//}
