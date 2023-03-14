d0b6c1f0ca1fea458e4f7e52431dd1ead1ff4279
EgorRevenga <127834856+EgorRevenga@users.noreply.github.com>
2023-03-14 14:50:47 +0300

    Update Product.tsx

--- components/Product.tsx
+++ components/Product.tsx
@@ -6,10 +6,21 @@ import { Price, ProductWithPrice } from "../types";
 import { postData } from "../utils/helpers";
 import { getStripe } from "../utils/stripe-client";
 import { useUser } from "../utils/useUser";
+import { GA } from '../services/google-analytics'
+
 export interface ProductProps {
   billingInterval: "year" | "month";
   product: ProductWithPrice;
 }
+
+// const wait = <T>(ms: number, returnValue?: T) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function wait<T>(ms: number, returnValue: T): Promise<T> {
+  return new Promise<T>((resolve) =>
+    setTimeout(() => resolve(returnValue), ms)
+  );
+}
+
 export const Product: React.FunctionComponent<
   PropsWithChildren<ProductProps>
 > = ({ product, billingInterval }) => {
@@ -19,6 +30,8 @@ export const Product: React.FunctionComponent<
   const { colorMode } = useColorMode();
 
   const handleCheckout = async (price: Price) => {
+    const scope = new Scope();
+
     setPriceIdLoading(price.id);
 
     if (!user) {
@@ -28,50 +41,69 @@ export const Product: React.FunctionComponent<
       return router.push("/account");
     }
 
+    const gaTimeout = 2000; // 2 seconds
     let analyticsClientId: string | null = null;
-    if (gtag) {
-      await new Promise((resolve) =>
-        gtag("event", "begin_checkout", {
-          event_callback: resolve,
-        })
-      );
 
-      analyticsClientId = await new Promise<string | null>((resolve) =>
-        gtag("get", GA4_ID, "client_id", (cid) => {
-          if (typeof cid === "string") {
-            resolve(cid);
-          }
-          resolve(null);
-        })
-      );
+    try {
+      await Promise.race([
+        await new Promise((resolve) =>
+          GA()?.("event", "begin_checkout", {
+            event_callback: resolve,
+          })
+        ),
+        wait(gaTimeout, null),
+      ]);
+
+      analyticsClientId = await Promise.race([
+        new Promise<string | null>((resolve) =>
+          GA()?.("get", GA4_ID, "client_id", (cid) => {
+            if (typeof cid === "string") {
+              resolve(cid);
+            }
+            resolve(null);
+          })
+        ),
+        wait(gaTimeout, null),
+      ]);
+    } catch (err) {
+      
     }
 
     try {
       const { sessionId } = await postData({
         url: "/api/create-checkout-session",
-  
         data: { price, analyticsClientId },
       });
 
       const stripe = await getStripe();
-      stripe?.redirectToCheckout({ sessionId });
+
+      if (!stripe) {
+       
+        return;
+      }
+
+      stripe.redirectToCheckout({ sessionId });
     } catch (error) {
-      return alert((error as Error)?.message);
+      
     } finally {
       setPriceIdLoading(undefined);
     }
   };
+
   const price = product?.prices?.find(
     (price) => price.interval === billingInterval
   );
+
   if (!price) {
     return null;
   }
+
   const priceString = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: price.currency,
     minimumFractionDigits: 0,
   }).format((price?.unit_amount || 0) / 100);
+
   return (
     <Box
       key={product.id}
@@ -102,7 +134,6 @@ export const Product: React.FunctionComponent<
         <Button
           disabled={isLoading}
           isLoading={priceIdLoading === price.id}
-          onClick={() => handleCheckout(price)}
           size="lg"
           width="100%"
           colorScheme="gray"
