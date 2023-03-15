4a2e58b8958d7c66c7335e0e9b38c9854a92ebeb
EgorRevenga <127834856+EgorRevenga@users.noreply.github.com>
2023-03-15 16:58:08 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,26 +13,6 @@ export default class Document extends NextDocument {
             src="https://stats.samesound.ru/js/script.js"
           />
 		  
-		  <Script 
-			type="text/javascript" 
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
-
-   ym(32834997, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-		/>
-		<noscript>
-			<div>
-				<img src="https://mc.yandex.ru/watch/32834997" style="position:absolute; left:-9999px;" alt="" />
-			</div>
-		</noscript>
-		  
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
