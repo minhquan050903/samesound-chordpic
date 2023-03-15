4e8af8a91993d1bdd0b1f86592e3dbc27777765e
EgorRevenga <127834856+EgorRevenga@users.noreply.github.com>
2023-03-15 14:37:16 +0300

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -182,7 +182,7 @@ const MenuLinks: React.FunctionComponent<{
 		{pathname !== "https://samesound.ru" && (
           <Box>
             <NextLink href="https://samesound.ru" target="_blank" passHref legacyBehavior>
-              <Button as="a" size="md" onClick={onCloseMenu} variant="solid">
+              <Button as="a" size="md" onClick={onCloseMenu} variant="solid-red-samesound">
                 На SAMESOUND →
               </Button>
             </NextLink>
