4b1397e89e8e877eee948eeeda64233cd026439a
EgorRevenga <127834856+EgorRevenga@users.noreply.github.com>
2023-03-16 11:01:04 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,7 +81,7 @@ const Home: NextPage = () => {
             >
               <Box p={3} id="editor" position="relative">
                 <Heading
-                  as="h2"
+                  as="h2 editor"
                   size="15px"
                   transform={[null, null, null, "rotate(-45deg)"]}
                   transformOrigin="0 0"
@@ -115,7 +115,7 @@ const Home: NextPage = () => {
             >
               <Box p={3} id="result" height="100%" position="relative">
                 <Heading
-                  as="h2"
+                  as="h2 result"
                   size="15px"
                   transform={[null, null, null, "rotate(-45deg)"]}
                   transformOrigin="0 0"
